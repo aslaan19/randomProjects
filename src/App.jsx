761136b0d570a389fdc1e4de71b611252ce908b3
Abdullah Aslan <abullahaslan3010@gmail.com>
2024-12-10 01:40:29 +0300

    back again

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useState } from 'react'
-import DarkLight from './Dark-light/DarkLight'
-import './App.css'
-import Scroll from './scroll_indicator/scroll'
-import Tabs from './tabs/Tabs'
-import ModelTest from './modelPop/modelTest'
-import Github from './github-finder/github'
-import Auto from './autoComplete/Auto'
-import Tictac from './tic-tac-toe/Tictac'
-function App() {
-
+import React from 'react';
+import './App.css';
+import "./github-finder/github"
+import Github from './github-finder/github';
+export default function UserProfile() {
   return (
-    <>
-      <Tictac></Tictac>
-    </>
-  )
+   Github()
+  );
 }
-
-export default App
